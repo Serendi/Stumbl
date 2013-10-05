8b249443f1b27ca8f11e2fcce3f87cae9284c5ae
Abhishek Gupta <atg.abhishek@gmail.com>
2013-10-05 17:25:09 -0400

    take a snap

--- gen/com/example/stumbl/R.java
+++ gen/com/example/stumbl/R.java
@@ -25,11 +25,11 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080005;
-        public static final int imageView1=0x7f080001;
-        public static final int imageView2=0x7f080002;
-        public static final int imageView3=0x7f080003;
-        public static final int imageView4=0x7f080004;
-        public static final int quickContactBadge1=0x7f080000;
+        public static final int button1=0x7f080004;
+        public static final int imageView1=0x7f080000;
+        public static final int imageView2=0x7f080003;
+        public static final int imageView3=0x7f080002;
+        public static final int imageView4=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
