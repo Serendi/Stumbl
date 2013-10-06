edbf776e935d648dbbc42cc0121ed8accb1cfa8c
Santiago Paiva <santiago.paiva@mail.mcgill.ca>
2013-10-05 21:04:08 -0400

    New commit

--- src/com/example/stumbl/MainActivity.java
+++ src/com/example/stumbl/MainActivity.java
@@ -1,50 +1,52 @@
 package com.example.stumbl;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Camera;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
+	ImageView iv; 
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        safeCameraOpen(1);
         setContentView(R.layout.activity_main);
-    }
-
     
-    private boolean safeCameraOpen(int id) {
-        boolean qOpened = false;
-      
-        try {
-            releaseCameraAndPreview();
-            mCamera = Camera.open(id);
-            qOpened = (mCamera != null);
-        } catch (Exception e) {
-            Log.e(getString(R.string.app_name), "failed to open Camera");
-            e.printStackTrace();
-        }
-
-        return qOpened;    
-    }
-
-    private void releaseCameraAndPreview() {
-        mPreview.setCamera(null);
-        if (mCamera != null) {
-            mCamera.release();
-            mCamera = null;
-        }
+        iv = (ImageView) findViewById(R.id.imageView); 
+        
+        Button btn = (Button) findViewById(R.id.takephoto);
+        btn.setOnClickListener(new OnClickListener()
+        {
+        	
+        	public void onClick(View w)
+        	{
+        		Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        		startActivityForResult(intent,0);	
+        		
+        	}
+        	
+        });       
+    
     }
     
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+    	if(requestCode == 0)
+    	{
+    		Bitmap theImage = (Bitmap) data.getExtras().get("data");
+    		iv.setImageBitmap(theImage);
+    		
+    	}
     }
     
 }
